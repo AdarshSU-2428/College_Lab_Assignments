574e8bc939b1b51b5985e25107180574c2c475cf
AdarshSU-2428 <adarshssahoo2006@gmail.com>
2026-08-27 22:27:16 +0530

    added assignment 2 where basic chat service is established between client and server.

---
+++ 5TH SEM ASSIGNMENTS/CN_LAB_ASSIGNMENT/ASSIGNMENT-2/serverchat.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+
+void error(char *msg)
+{
+	perror(msg);
+	exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+	int sockfd, newsockfd,portno,clilen;
+	char buffer[256];
+	struct sockaddr_in serv_addr,cli_addr;
+	int n;
+
+	if(argc<2) {
+		fprintf(stderr,"ERROR, no port provided\n");
+		exit(1);
+	}
+
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
+
+	if(sockfd<0) {
+		error("ERROR opening socket");
+	}
+
+	bzero((char *) &serv_addr,sizeof(serv_addr));
+	portno=atoi(argv[1]);
+	serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr=INADDR_ANY;
+	serv_addr.sin_port=htons(portno);
+	
+	if (bind(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr))<0) {
+		error("binding ERROR");
+	}
+
+	listen(sockfd,5);
+	clilen=sizeof(cli_addr);
+	newsockfd=accept(sockfd,(struct sockaddr *) &cli_addr,&clilen);
+	
+	while(1) {		
+		if(newsockfd<0) {
+			error("ERROR on accept");
+		}
+		bzero(buffer,256);
+		n=read(newsockfd,buffer,255);
+		
+		if(n<0) {
+			error("ERROR reading from socket");
+		}
+
+		printf("Here is the message: %s\n",buffer);
+		printf("Enter your message:");
+		bzero(buffer,256);
+		fgets(buffer,256,stdin);
+		
+		if(buffer[0]=='b' && buffer[1]=='y' && buffer[2]=='e') {
+				exit(1);
+		}
+
+		n=write(newsockfd,buffer,strlen(buffer));
+		if (n<0) error("Error writing to socket\n");
+	}
+	return 0;
+}
